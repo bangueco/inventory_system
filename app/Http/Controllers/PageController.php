cd61600903d93d73201e645f51b555b37ece1dee
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-10-31 17:59:31 +0800

    Add controller that controls page views

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function viewDashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function viewManageProducts()
+    {
+        return view('manage_products');
+    }
+
+    public function viewLogin()
+    {
+        return view('Authentication.login');
+    }
+}
