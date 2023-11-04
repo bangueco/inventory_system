d30cc755da499df08ce292104da7ac42d81bf877
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-11-04 12:55:27 +0800

    Show validation errors

--- resources/views/manage_products.blade.php
+++ resources/views/manage_products.blade.php
@@ -2,17 +2,29 @@
 
 @section('title', 'Manage Products')
 
+@section('links')
+<link rel="stylesheet" href="{{ asset('assets/css/manage_products.css') }}">
+@endsection
+
 @section('content')
 <div class="container">
-  <form class="p-3" action="{{ route('manage_products.add') }}" method="post">
+  <form class="p-3 container" action="{{ route('manage_products.add') }}" method="post">
     @csrf
-    <div class="input-group mb-3">
-      <span class="input-group-text">Product Name:</span>
-      <input class="form-control" type="text" name="product_name" id="product_name">
-    </div>
-    <div class="input-group mb-3">
-      <span class="input-group-text">Quantity:</span>
-      <input class="form-control" type="number" name="product_quantity" id="product_quantity">
+    <div>
+      <div class="form-floating mb-3">
+        <input class="form-control" type="text" name="product_name" id="product_name" placeholder="Product Name">
+        <label for="product_name">Product Name</label>
+        @error('product_name')
+        <p class="text text-danger"> {{ $message }} </p>
+        @enderror
+      </div>
+      <div class="form-floating mb-3">
+        <input class="form-control" type="number" name="product_quantity" id="product_quantity" placeholder="Product Quantity">
+        <label for="product_quantity">Product Quantity</label>
+        @error('product_quantity')
+        <p class="text text-danger"> {{ $message }} </p>
+        @enderror
+      </div>
     </div>
     <button class="btn btn-success">Submit</button>
   </form>
